81c24f84ddf4cc48700b89c20f6feca274e87150
Devan Erza <devanersa6@gmail.com>
2026-09-01 16:06:41 +0700

    feat: add activity model

---
+++ app/Models/Activity.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'application_id',
+        'user_id',
+        'type',
+        'title',
+        'description',
+        'activity_date',
+    ];
+
+    protected $casts = [
+        'activity_date' => 'date',
+    ];
+
+    public function application()
+    {
+        return $this->belongsTo(Application::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
